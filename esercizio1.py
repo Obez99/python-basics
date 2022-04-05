d42174a2f75fa908b0c02f8640a12bf4d2ee7229
Obez99 <giuliogx@live.it>
2022-04-05 18:15:36 +0200

    Completed exercise

---
+++ esercizio1.py
@@ -0,0 +1,8 @@
+print('\n\nBenvenuto gioia, questo programma moltiplica due numeri\n')
+
+x = float(input('Inserisci il primo valore ---> '))
+y = float(input('Inserisci il secondo valore ---> '))
+
+z = x * y
+
+print('Il risultato della moltiplicazione è: ' + str(z))
